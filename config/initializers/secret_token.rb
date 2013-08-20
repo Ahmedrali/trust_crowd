3a07158192716a97ee72c84f66b8e6839c5a03dd
Ahmedrali <ahmedrali2@gmail.com>
2013-08-20 10:44:06 -0700

    - Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TrustCrowd::Application.config.secret_key_base = '7068a8d9f345d56806dd98d5d53e48e7cb401cc5d3fec0cc3688114bf364a2270c0cbfcac8e8f496b8e082a405c2d84bc3838dc3e63fad4faae5f20bb35a8e81'
